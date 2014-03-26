702beeff88cc78e161f380a0ec194bf4f0d54f89
bjprat <bjprat@gmail.com>
2014-03-26 15:19:04 -0400

    Still troubleshooting aws, updated pin.rb w/ s3 host name again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,9 +88,10 @@ config.paperclip_defaults = {
   :s3_credentials => {
     :bucket => ENV['S3_BUCKET_NAME'],
     :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
+    :s3_host_name = 's3-us-west-2.amazonaws.com'
   }
-  :s3_host_name = 's3-us-west-2.amazonaws.com'
+
 }
 end
 
